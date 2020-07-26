98628306ff1347e3064a2172658f1d27787c9e67
mmo <mmo@kubee.ga>
2020-07-26 18:05:45 +0300

    Update Version: 1.0.0

---
+++ src/Exceptions/ConstantAlreadyDefinedException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Kubecms\CMSConfig\Exceptions;
+
+/**
+ * Class ConstantAlreadyDefinedException
+ * This should be thrown when a user attempts to define() a constant that has already been defined
+ * @package Kubecms\Kcms
+ */
+class ConstantAlreadyDefinedException extends \RuntimeException
+{
+
+}
---
+++ src/Exceptions/UndefinedConfigKeyException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Kubecms\CMSConfig\Exceptions;
+
+class UndefinedConfigKeyException extends \RuntimeException
+{
+  
+}
